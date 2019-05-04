b05db7f359d3852d2e4e28a32634e9e76245eb0b
Albo Vieira <albovieira@gmail.com>
2019-05-04 11:45:17 -0300

    fix test

--- src/test/moviedb.spec.ts
+++ src/test/moviedb.spec.ts
@@ -4,24 +4,39 @@ import { Container } from 'inversify';
 import * as chai from 'chai';
 import { expect } from 'chai';
 import * as sinon from 'sinon';
+import * as nock from 'nock';
+import 'chai-http';
 import chaiAsPromised = require('chai-as-promised');
 import * as moment from 'moment';
+import createApp from '../lib/app';
 import bootstrap from '../lib/bootstrap';
+import { ServiceConfig } from '../lib/model';
+import { TYPES } from '../lib/util/ioc-types';
 
 chai.use(chaiAsPromised);
+chai.use(require('chai-http'));
 
 describe('MovieDB', () => {
-  it('Deve criar uma intenção de busca', async () => {
-    nock()
-      .get('/airports/stretch/CNF/GRU')
-      .reply(HttpStatus.OK, {
-        route: {
-          from: { iata: 'CNF', country: 'BR' },
-          to: { iata: 'GRU', country: 'BR' },
-          international: false,
-          mercosul: false,
-          airlines: ['azul'],
-        },
+  let movieConfig: ServiceConfig;
+
+  before(async () => {
+    const container = await bootstrap();
+    movieConfig = container.get(TYPES.TheMovieDBConfig);
+  });
+
+  it('should get upcoming movies', async () => {
+    nock(movieConfig.url)
+      .get('/movie/upcoming')
+      .reply(200, {
+        done: true,
       });
+
+    const container = await bootstrap();
+    const app = await createApp(container);
+    const { status, body } = await chai
+      .request(app)
+      .get('/movies/')
+      .query({ page: 1 });
+    expect(status).to.be.eq(200);
   });
 });
--- src/test/setup.spec.ts
+++ src/test/setup.spec.ts
@@ -2,9 +2,3 @@ import 'reflect-metadata';
 import * as dotenv from 'dotenv';
 
 dotenv.config({ path: './.env.test' });
-
-import bootstrap from '../src/bootstrap';
-
-before(async () => {
-  // const container = await bootstrap();
-});
